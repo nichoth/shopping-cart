4b30f7ebb2003afd6c0a094527a27b1ba9628b9d
nichoth <nichoth@gmail.com>
2020-07-11 14:51:25 -0700

    trying hooks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import { h, render } from 'preact'
 import htm from 'htm'
+import { useState } from 'preact/hooks'
 var html = htm.bind(h)
 
 function Example (props) {
-    return html`<div>example</div>`
+    var [count, setCount] = useState(0)
+
+    setInterval(function () {
+        setCount(count + 1)
+    }, 1000)
+
+    return html`<div>
+        Count: ${count}
+    </div>`
 }
 
 render(html`<${Example} />`, document.getElementById('content'))
