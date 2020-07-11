2b7fe6ed76961dccf811e36db4731c243329e4e5
nichoth <nichoth@gmail.com>
2020-07-11 15:56:22 -0700

    works in the old style

--- src/index.js
+++ src/index.js
@@ -1,18 +1,54 @@
-import { h, render } from 'preact'
+import { h, render, Component } from 'preact'
 import htm from 'htm'
-import { useState } from 'preact/hooks'
+// import { useState } from 'preact/hooks'
 var html = htm.bind(h)
+var Bus = require('@nichoth/events')
+// var observ = require('observ')
+var struct = require('observ-struct')
 
-function Example (props) {
-    var [count, setCount] = useState(0)
+var state = struct({
+    count: 0
+})
 
-    setInterval(function () {
-        setCount(count + 1)
-    }, 1000)
+var bus = Bus({
+    memo: true
+})
 
-    return html`<div>
-        Count: ${count}
-    </div>`
+function emit () {
+    return bus.emit.apply(bus, arguments)
+}
+
+function subscribe (bus, state) {
+    bus.on('plus', function () {
+        console.log('plus')
+        state.set({
+            count: state().count + 1
+        })
+    })
+}
+
+subscribe(bus, state)
+
+class Example extends Component {
+    constructor(props) {
+        super(props)
+        this.state = state()
+    }
+
+    componentDidMount () {
+        setInterval(function () {
+            emit('plus', null)
+        }, 1000)
+
+        var self = this
+        state(function onChange (data) {
+            self.setState(data)
+        })
+    }
+
+    render () {
+        return html`<div>Count: ${this.state.count} <//>`
+    }
 }
 
 render(html`<${Example} />`, document.getElementById('content'))
