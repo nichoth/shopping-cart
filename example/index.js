b30614d8122494bcd111471a4179574771794f55
nichoth <nichoth@gmail.com>
2020-07-11 20:20:32 -0700

    stuff

--- example/index.js
+++ example/index.js
@@ -1,4 +1,8 @@
 import Icon from '../src/icon'
 
+// pass in an element to mount it at
+// or it will automatically mount to the given ID
 Icon(document.getElementById('shopping-cart-icon'))
+
+// can also call window.Icon()
 
