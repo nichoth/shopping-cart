7c322acaf1fa988b47e3b167380d0fc37d968815
nichoth <nichoth@gmail.com>
2020-07-15 22:10:23 -0700

    test

--- test-browser/index.js
+++ test-browser/index.js
@@ -1,7 +1,11 @@
 var test = require('tape')
+import Cart from '../src/cart'
+
+var cart = new Cart({ storage: false })
 
 test('example', function (t) {
-    t.pass('example')
+    t.ok(cart, 'should create cart')
+    cart.createIcon()
     t.end()
 })
 
